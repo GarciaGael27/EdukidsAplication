a0b3ad784752e4d64ec2722bc4305371b6f64282
Toph <fenshu28@gmail.com>
2025-06-02 14:55:42 -0600

    Actualización de login y registro

--- app/src/main/java/com/example/edukidsaplication/Login.kt
+++ app/src/main/java/com/example/edukidsaplication/Login.kt
@@ -12,12 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
