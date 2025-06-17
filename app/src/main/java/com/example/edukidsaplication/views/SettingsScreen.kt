ba0ef6eae94d9ba4aa7b206f621bcdb7eca841e6
KIMNERAY <garciapinachogael@gmail.com>
2025-06-17 06:01:09 -0600

    Actualización de versiones de dependencias y mejora en la importación de iconos en la pantalla de configuración

--- app/src/main/java/com/example/edukidsaplication/views/SettingsScreen.kt
+++ app/src/main/java/com/example/edukidsaplication/views/SettingsScreen.kt
@@ -10,18 +10,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -98,7 +97,7 @@ fun SettingsScreen(
                             }
                         )
 
-                        Divider()
+                        HorizontalDivider()
 
                         ListItem(
                             headlineContent = { Text(homeState.user.username) },
@@ -152,7 +151,7 @@ fun SettingsScreen(
                         )
                     ) {
                         Icon(
-                            Icons.Default.ExitToApp,
+                            Icons.AutoMirrored.Filled.ExitToApp,
                             contentDescription = "Cerrar Sesión",
                             tint = Color.White
                         )
